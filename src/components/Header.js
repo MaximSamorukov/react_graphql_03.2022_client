85d32db1473aec524fe9321b5b50923646383c2e
Maxim Samorukov <maxim.samorukov@gmail.com>
2022-03-09 23:12:02 +0300

    second commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Button type="primary">Add</Button>
+    </div>
+  )
+}
+
+export default Header;
